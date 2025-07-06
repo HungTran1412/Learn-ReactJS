67e78a21f343c417ec6bd0a0ceb49e9987bca2e6
Minh Hùng <hug2k4123@gmail.com>
2025-07-06 15:46:14 +0700

    learn component

---
+++ src/views/example/mycomponent.js
@@ -0,0 +1,11 @@
+ import React from "react";
+
+ class MyComponent extends React.Component{
+    render(){
+        return (
+            <div>Hello my Component tnmhung</div>
+        )    
+    }
+ }
+
+ export default MyComponent;
