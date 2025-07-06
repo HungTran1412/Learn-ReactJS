1a9c53a43805bfb10ee7361eb9b75393b67f8fb8
Minh Hùng <hug2k4123@gmail.com>
2025-07-06 16:12:53 +0700

    state in react

--- src/views/example/mycomponent.js
+++ src/views/example/mycomponent.js
@@ -1,11 +1,32 @@
- import React from "react";
+import React from "react";
 
- class MyComponent extends React.Component{
-    render(){
-        return (
-            <div>Hello my Component tnmhung</div>
-        )    
+class MyComponent extends React.Component {
+
+    state = {
+        name: 'Mhung',
+        channel: 'Atoets'
     }
- }
 
- export default MyComponent;
+    /*
+    JSX =>
+    */
+    render() {
+
+        let name = 'Mhung';
+
+        return (
+            // <React.Fragment>
+            <>
+                <div className="1">
+                    Hello my Component, my name is {this.state.name}
+                </div>
+                <div className="2">
+                    My ex: {this.state['channel']}
+                </div>
+            </>
+            // </React.Fragment>
+        )
+    }
+}
+
+export default MyComponent;
