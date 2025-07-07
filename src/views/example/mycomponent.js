6d8f112f344d25c4c4d6fe4837a345e06f2ac1d7
Minh Hùng <hug2k4123@gmail.com>
2025-07-07 16:50:55 +0700

    DOM Event Handle

--- src/views/example/mycomponent.js
+++ src/views/example/mycomponent.js
@@ -7,6 +7,17 @@ class MyComponent extends React.Component {
         channel: 'Atoets'
     }
 
+    handleOnChangeName = (event) =>{
+        this.setState({
+            name: event.target.value
+        })
+    }
+
+    handleClickButton = () =>{
+        console.log('hihifsdf');
+        alert('click me')
+    }
+
     /*
     JSX =>
     */
@@ -18,10 +29,16 @@ class MyComponent extends React.Component {
             // <React.Fragment>
             <>
                 <div className="1">
+                    <input value={this.state.name} type="text"
+                        onChange={(event) => this.handleOnChangeName(event)}
+                    />
                     Hello my Component, my name is {this.state.name}
                 </div>
                 <div className="2">
                     My ex: {this.state['channel']}
+                </div>
+                <div className="3">
+                    <button onClick={this.handleClickButton}>Click me </button>
                 </div>
             </>
             // </React.Fragment>
