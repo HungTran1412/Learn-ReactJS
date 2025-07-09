5d2b983a7fb8495c58cf4a4cd790afcc273be616
Minh Hùng <hug2k4123@gmail.com>
2025-07-09 20:10:32 +0700

    setState and re-render

--- src/views/example/mycomponent.js
+++ src/views/example/mycomponent.js
@@ -3,26 +3,30 @@ import React from "react";
 class MyComponent extends React.Component {
 
     state = {
-        name: 'Mhung',
-        channel: 'Atoets'
+        name: '',
+        ex: 'Atoets'
     }
 
     handleOnChangeName = (event) =>{
+        // console.log(event.target.value, 'event target: ', event.target, 'event object: ', event)
+
+        // this.state.name = event.target.value; //không nên dùng
+
         this.setState({
-            name: event.target.value
+            name: event.target.value,
+            ex: 'at'
         })
     }
 
     handleClickButton = () =>{
-        console.log('hihifsdf');
         alert('click me')
     }
-
+ 
     /*
     JSX =>
     */
     render() {
-
+        console.log('>>> call render: ', this.state)
         // let name = 'Mhung';
 
         return (
@@ -35,7 +39,7 @@ class MyComponent extends React.Component {
                     Hello my Component, my name is {this.state.name}
                 </div>
                 <div className="2">
-                    My ex: {this.state['channel']}
+                    My ex: {this.state['ex']}
                 </div>
                 <div className="3">
                     <button onClick={this.handleClickButton}>Click me </button>
